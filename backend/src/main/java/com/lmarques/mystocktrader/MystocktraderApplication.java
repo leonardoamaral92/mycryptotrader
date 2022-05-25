99cb44593351019a80a7081133ea16b0e59a180d
Leonardo <leonardo.amaral92@gmail.com>
2022-05-25 20:10:30 -0300

    Criando projeto backend

---
+++ backend/src/main/java/com/lmarques/mystocktrader/MystocktraderApplication.java
@@ -0,0 +1,14 @@
+package com.lmarques.mystocktrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystocktraderApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MystocktraderApplication.class, args);
+	}
+
+}
