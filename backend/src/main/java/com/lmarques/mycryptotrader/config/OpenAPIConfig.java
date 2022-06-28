faa85ce2750c0edd5f86307466f59109482322d7
Leonardo <leonardo.amaral92@gmail.com>
2022-06-28 16:23:46 -0300

    Adicionando documentação default do OpenAPI

---
+++ backend/src/main/java/com/lmarques/mycryptotrader/config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.lmarques.mycryptotrader.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MyCryptoTrader")
+                        .version("v1")
+                        .description("Gerenciador de portfólio de criptomoedas")
+                        .termsOfService("")
+                        .license(new License().name("Apache 2.0")))
+                ;
+    }
+
+
+}
